51df4200843b7df7d088a09146b910d7854c8064
crisjulyancolesco <crisjulyan11@gmail.com>
2026-02-20 16:43:31 +0800

    fix: manual changes (change tailwind version, and remove webpack config)

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   // Tailwind CSS can work with this baseline; additional config can be added if needed
-  webpack: (config) => {
-    return config;
-  }
+
 };
 module.exports = nextConfig;
